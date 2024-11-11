e39a1347a67860578b303ec2032dc215c5ca0fc1
reedcowan <reed.cowan@shipyardapp.com>
2024-11-11 09:19:51 -0500

    Started with employee unit tests

--- app/domain/employee/employeesvc/service_test.go
+++ app/domain/employee/employeesvc/service_test.go
@@ -1,6 +1,15 @@
 package employeesvc
 
-import "testing"
+import (
+	"context"
+	"errors"
+	"log"
+	"testing"
+	"timeCardSimple/App/domain/employee/employeetest"
+	"timeCardSimple/app/domain/employee"
+
+	"github.com/golang/mock/gomock"
+)
 
 var (
 	firstName = "First"
@@ -8,6 +17,105 @@ var (
 	email     = "test@email.com"
 )
 
-func Test_CreateEmployee_Success(t *testing.T) {
+func createEmployeeParams() employee.CreateParams {
+	return employee.CreateParams{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+	}
+}
 
+func Test_CreateEmployee_AddRepoErr(t *testing.T) {
+	mc := gomock.NewController(t)
+	defer mc.Finish()
+
+	errAddEmployee := errors.New("Add Employee Error")
+
+	employeeRepo := employeetest.NewMockRepo(mc)
+	employeeRepo.EXPECT().AddEmployee(gomock.Any(), gomock.Any()).Return(errAddEmployee)
+
+	s := &Service{
+		employeeRepo: employeeRepo,
+	}
+
+	createParams := createEmployeeParams()
+
+	_, err := s.CreateEmployee(
+		context.Background(),
+		createParams,
+	)
+
+	if err != errAddEmployee {
+		t.Errorf("incorrect error, received: %v, expected: %v", err, errAddEmployee)
+	}
+}
+
+func Test_CreateEmployee_Success(t *testing.T) {
+	mc := gomock.NewController(t)
+	defer mc.Finish()
+
+	employeeRepo := employeetest.NewMockRepo(mc)
+	employeeRepo.EXPECT().AddEmployee(gomock.Any(), gomock.Any()).Return(nil)
+
+	s := &Service{
+		employeeRepo: employeeRepo,
+	}
+
+	createParams := createEmployeeParams()
+
+	e, err := s.CreateEmployee(
+		context.Background(),
+		createParams,
+	)
+
+	if err != nil {
+		t.Errorf("incorrect error, received: %v, expected: %v", err, nil)
+	}
+
+	if e == nil {
+		t.Errorf("incorrect employee, recieved: %v, expected: %v", e, &employee.Employee{})
+	}
+}
+
+func Test_GetEmployees_Sucess(t *testing.T) {
+	mc := gomock.NewController(t)
+	defer mc.Finish()
+
+	createParams1 := createEmployeeParams()
+	createParams2 := employee.CreateParams{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "fake@test.com",
+	}
+
+	employee1, err := employee.New(createParams1)
+	if err != nil {
+		log.Fatalf("Error creating employee1")
+	}
+
+	employee2, err := employee.New(createParams2)
+	if err != nil {
+		log.Fatalf("Error creating employee2")
+	}
+
+	expectedEmployees := []*employee.Employee{employee1, employee2}
+
+	employeeRepo := employeetest.NewMockRepo(mc)
+	employeeRepo.EXPECT().GetAllEmployees(gomock.Any()).Return(expectedEmployees, nil)
+
+	s := &Service{
+		employeeRepo: employeeRepo,
+	}
+
+	receivedEmployees, err := s.GetEmployees(
+		context.Background(),
+	)
+
+	if err != nil {
+		t.Errorf("incorrect error, received: %v, expected: %v", err, nil)
+	}
+
+	if len(receivedEmployees) != len(expectedEmployees) {
+		t.Errorf("incorrect number of employees returned, receivced: %v, expected: %v", len(receivedEmployees), len(expectedEmployees))
+	}
 }
--- go.mod
+++ go.mod
@@ -14,8 +14,4 @@ require (
 require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/tools v0.1.1 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
